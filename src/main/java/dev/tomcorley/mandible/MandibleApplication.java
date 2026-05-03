9051fb58ba2d0791e728900a255b7a574ed888df
Tom Corley <tomcorley86@gmail.com>
2026-05-03 18:28:26 +0100

    spring boot setup.

---
+++ src/main/java/dev/tomcorley/mandible/MandibleApplication.java
@@ -0,0 +1,13 @@
+package dev.tomcorley.mandible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tomcorley/mandible/MandibleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tomcorley.mandible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
